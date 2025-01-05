cb0cfb10001e360e70a5bdf0ffb99413219675eb
Teemu Siikaniemi <teemu.siikaniemi@gmail.com>
2025-01-05 13:44:18 +0200

    fix auth.ts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,8 +3,7 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    // return null;
-    return "nopt ok";
+    return null;
   }
 
   const splitAuth = authHeader.split(" ");
